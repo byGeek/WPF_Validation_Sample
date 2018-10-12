f5bf84cbaed49034b999b7cf9e9ea9e76d8b9dc3
byGeek <byyang@mail.dlut.edu.cn>
2018-10-12 15:17:36 +0800

    initial commit

---
+++ src/WpfValidationDemo/WpfValidationDemo/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfValidationDemo
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly Person person;
+        public MainViewModel(Person p)
+        {
+            person = p;
+        }
+
+        public string UserName
+        {
+            get { return person.UserName; }
+            set
+            {
+                person.UserName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        public string Password
+        {
+            get { return person.Password; }
+            set
+            {
+                person.Password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+    }
+}
---
+++ src/WpfValidationDemo/WpfValidationDemo/NoNumberRule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfValidationDemo
+{
+    public class NoNumberRule : ValidationRule
+    {
+        public NoNumberRule()
+        {
+
+        }
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            string username = string.Empty;
+
+            try
+            {
+                username = value.ToString();
+                var arr = username.ToCharArray();
+                foreach(var ch in arr)
+                {
+                    if(ch > '0' && ch < '9')
+                    {
+                        return new ValidationResult(false, "Can not accept number!");
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                return new ValidationResult(false, e.Message);
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
